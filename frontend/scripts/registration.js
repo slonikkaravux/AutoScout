1c8a2344de44781a9cbfd679c9524f4cc4333392
Zaikin Volodymyr <118755479+slonikkaravux@users.noreply.github.com>
2023-11-17 16:14:26 +0100

    Registration

--- frontend/scripts/registration.js
+++ frontend/scripts/registration.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("Registration page loaded.");
 });
+
+
+function togglePasswordVisibility() { 
+  var passwordInput = document.getElementById("password"); 
+  passwordInput.type = (passwordInput.type === "password") ? "text" : "password"; 
+} 
